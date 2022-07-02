f998721981d62f45a029ddaaa0efafa2924a2e7b
Increase_Lois <increasebruce@gmail.com>
2022-07-02 07:48:27 -0700

    Updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - Prints all possible combinations of two different digits,
 *        in ascending order, separated by a comma followed by a space.
@@ -9,7 +8,6 @@
 int main(void)
 {
 int digit1, digit2;
-
 for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -18,14 +16,11 @@ putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 if (digit1 == 8 && digit2 == 9)
 continue;
-		
 putchar(',');
 putchar(' ');
 }
 }
-
 putchar('\n');
-
 return (0);
 }
 
