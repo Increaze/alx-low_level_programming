d4444acac775a108efd081e19d9e785f4d905061
Increase_Lois <increasebruce@gmail.com>
2022-07-02 07:33:43 -0700

    Variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Description - Printing base 16 numbers
- *Return:0
- */
+*main - Entry point
+*Description - Printing base 16 numbers
+*Return:0
+*/
 int main(void)
 {
     int i, y;
