c72bc41e0d5a9a6efa8cdc08765e586760e89edc
Increase_Lois <increasebruce@gmail.com>
2022-07-06 11:47:22 -0700

    Functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
-* main - prints putchar
-* Return: always 0
-*/
+ * main - prints "_putchar"
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char ch[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(ch[i]);
 	_putchar('\n');
 
 	return (0);
