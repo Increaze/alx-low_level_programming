ffb2123a6c2457488fdd66aaddd7ac67efefc54f
Increase_Lois <increasebruce@gmail.com>
2022-07-15 02:15:56 -0700

    Updated

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,19 @@
 #include "main.h"
-
-void print_uint(unsigned int n);
-
-/**
- * print_number - Prints an integer
- * @n: The integer to print
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	print_uint((unsigned int) n);
-}
-
-/**
- * print_uint - Prints an unsigned integer
- * @n: The unsigned int to print
- */
-void print_uint(unsigned int n)
-{
-	if (n / 10 != 0)
-		print_uint(n / 10);
-	_putchar(n % 10 + '0');
+/** 
+ *print_number - prints integers 
+ *@n: number to be printed 
+ *Return: nothing 
+ */ 
+void print_number(int n) 
+{ 
+        unsigned int x = n; 
+ 
+        if (n < 0) 
+        { 
+                _putchar('-'); 
+                x = -x; 
+        } 
+        if ((x / 10) > 0) 
+                print_number(x / 10); 
+        _putchar((x % 10) + '0'); 
 }
